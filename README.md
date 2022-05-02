9f59472c518d8f9c86f2c6749d4f920ba9512e2b
Yeji Jang <yejijang95@gmail.com>
2022-05-01 21:21:02 -0400

    player added & able to move around

---
+++ README.md
@@ -0,0 +1,9 @@
+This is my very wack knock-off Pokemon Game - Fokemon!!! :)
+with veryyyyy limited functionality
+Atm - only the map works!
+
+I used Phaser & Tiled to create this.
+
+I used this article - https://medium.com/@michaelwesthadley/modular-game-worlds-in-phaser-3-tilemaps-1-958fc7e6bbd6
+
+I used this repo for the assets - https://github.com/Tuxemon/Tuxemon
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const config = {
   height: 600,
   parent: "game-container",
   pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
   scene: {
     preload: preload,
     create: create,
@@ -16,10 +22,16 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image("tiles", "./assets/tilesets/tuxmon-sample-32px-extruded.png");
   this.load.tilemapTiledJSON("map", "./assets/tilemaps/yeji-town.json");
+
+  this.load.atlas(
+    "atlas",
+    "./assets/atlas/tuxemon-girl/atlas.png",
+    "./assets/atlas/tuxemon-girl/atlas.json"
+  );
 }
 
-let controls;
 let player;
+let cursors;
 
 function create() {
   const map = this.make.tilemap({ key: "map" });
@@ -29,19 +41,112 @@ function create() {
   const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
   const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
 
-  const camera = this.cameras.main;
-  const cursors = this.input.keyboard.createCursorKeys();
-  controls = new Phaser.Cameras.Controls.FixedKeyControl({
-    camera: camera,
-    left: cursors.left,
-    right: cursors.right,
-    up: cursors.up,
-    down: cursors.down,
-    speed: 0.5,
+  worldLayer.setCollisionByProperty({ collides: true });
+  aboveLayer.setDepth(10);
+  const spawnPoint = map.findObject(
+    "Objects",
+    (obj) => obj.name === "Spawn Point"
+  );
+
+  player = this.physics.add
+    .sprite(spawnPoint.x, spawnPoint.y, "atlas", "37707_female_front")
+    .setSize(30, 40)
+    .setOffset(0, 24);
+  this.physics.add.collider(player, worldLayer);
+
+  const anims = this.anims;
+  anims.create({
+    key: "37707_female_left_walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "37707_female_left_walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3,
+    }),
+    frameRate: 10,
+    repeat: -1,
   });
+  anims.create({
+    key: "37707_female_right_walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "37707_female_right_walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  anims.create({
+    key: "37707_female_front_walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "37707_female_front_walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  anims.create({
+    key: "37707_female_back_walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "37707_female_back_walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  const camera = this.cameras.main;
+  camera.startFollow(player);
   camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update(time, delta) {
-  controls.update(delta);
+  const speed = 175;
+  const prevVelocity = player.body.velocity.clone();
+
+  player.body.setVelocity(0);
+
+  //horizontal movement
+  if (cursors.left.isDown) {
+    player.body.setVelocityX(-speed);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(speed);
+  }
+
+  //vertical movement
+  if (cursors.up.isDown) {
+    player.body.setVelocityY(-speed);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(speed);
+  }
+
+  player.body.velocity.normalize().scale(speed);
+
+  if (cursors.left.isDown) {
+    player.anims.play("37707_female_left_walk", true);
+  } else if (cursors.right.isDown) {
+    player.anims.play("37707_female_right_walk", true);
+  } else if (cursors.up.isDown) {
+    player.anims.play("37707_female_back_walk", true);
+  } else if (cursors.down.isDown) {
+    player.anims.play("37707_female_front_walk", true);
+  } else {
+    player.anims.stop();
+
+    // If we were moving, pick an idle frame to use
+    if (prevVelocity.x < 0) player.setTexture("atlas", "37707_female_left");
+    else if (prevVelocity.x > 0)
+      player.setTexture("atlas", "37707_female_right");
+    else if (prevVelocity.y < 0)
+      player.setTexture("atlas", "37707_female_back");
+    else if (prevVelocity.y > 0)
+      player.setTexture("atlas", "37707_female_front");
+  }
 }
